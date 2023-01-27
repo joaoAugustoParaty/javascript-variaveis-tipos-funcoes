355640b5b6a612d17fd809701fdc70789422ebab
joaoAugustoParaty <joaodrow@gmail.com>
2023-01-27 17:39:03 -0300

    First commit

---
+++ type-number.js
@@ -0,0 +1,30 @@
+// tipo Number
+
+const meuNumero = 3;
+
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+const somaMatematica = primeiroNumero + segundoNumero;
+console.log(somaMatematica);
+
+const multiplicacaoMatematica = primeiroNumero * segundoNumero;
+console.log(multiplicacaoMatematica);
+
+const divisaoMatematica = primeiroNumero / segundoNumero;
+console.log(divisaoMatematica);
+
+const numeroPontoFlutuante = 3.3;
+const numeroFlutuanteSemZero = .5;
+
+const novaOperacao = primeiroNumero /   numeroPontoFlutuante; 
+
+console.log(novaOperacao);
+
+// NaN -> Not A Number (não é um número);
+
+const alura = "Alura";
+console.log(alura * primeiroNumero);
+
+
+
